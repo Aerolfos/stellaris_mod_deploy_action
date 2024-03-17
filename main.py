587f69391472dd7323a77f2a5adb2dab5880830b
Aerolfos <Aerolfos@users.noreply.github.com>
2024-03-17 04:51:31 +0100

    set up workflow chain into py file

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import argparse
 #from pathlib import Path
+
+# handle command line inputs
+# the user shouldn't even see these, they're for the github action to call
+parser = argparse.ArgumentParser()
+possible_version_types = ["Major", "Minor", "Patch"]
+parser.add_argument("versionType", type=str, choices=possible_version_types, help="version type to bump")
+parser.add_argument("versionStellaris", type=str, help="Stellaris version to support")
 
 print("init")
