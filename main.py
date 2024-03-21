678447210cc85cd6643876d4d566ea187d7d03e8
Aerolfos <8443014+Aerolfos@users.noreply.github.com>
2024-03-21 03:14:09 +0100

    add file ingest and regex pattern for matching
    still messy, should properly separate methods into functions
    
    just need functionality written down

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 from methods.input_methods import str2bool
 
 # handle command line inputs
@@ -14,3 +15,29 @@ args = parser.parse_args()
 print("init")
 print(args)
 print("useChangelog:", args.useChangelog)
+
+# match the paradox descriptor version line, should be of form version="1.2.3", wildcards * are allowed
+regex_version_pattern = r"version=\"((?:(?:\d{1,3}|\*)\.){2}(?:\d{1,3}|\*))\"" # yeah regex be like that
+
+with open("descriptor", "r", encoding="utf-8") as f:
+    descriptor_data = f.read()
+
+with open("descriptor", "w", encoding="utf-8") as f:
+    f.write(descriptor_data)
+
+def replace(file, pattern, subst):
+    # Read contents from file as a single string
+    file_handle = open(file, 'r', encoding="utf-8")
+    file_string = file_handle.read()
+    file_handle.close()
+
+    if match := re.search(pattern, text, re.IGNORECASE):
+        mod_version = match.group(1)
+    # Use RE package to allow for replacement (also allowing for (multiline) REGEX)
+    file_string = (re.sub(pattern, subst, file_string))
+
+    # Write contents to file.
+    # Using mode 'w' truncates the file.
+    file_handle = open(file, 'w', encoding="utf-8")
+    file_handle.write(file_string)
+    file_handle.close()
