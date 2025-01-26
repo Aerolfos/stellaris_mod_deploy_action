3a02826bd92546e0e81669fbe24ef83168faac2b
Aerolfos <Aerolfos@users.noreply.github.com>
2025-01-26 01:58:14 +0100

    work on login woes

--- steam_workshop_upload.py
+++ steam_workshop_upload.py
@@ -6,16 +6,15 @@ import sys
 
 # get environment variables
 steam_home = os.getenv('STEAM_HOME', os.path.join(os.getenv('HOME'), 'Steam'))
+print(os.getenv('HOME'))
 root_path = os.getenv('rootPath', '')
 content_root = os.path.join(os.getcwd(), root_path)
 manifest_path = os.path.join(os.getcwd(), 'manifest.vdf')
 
-app_id = os.getenv('appId', '')
-item_id = os.getenv('itemId', '')
-change_note = os.getenv('changeNote', '')
-steam_totp = os.getenv('steam_totp', '')
+app_id = os.getenv('appID', '')
+item_id = os.getenv('itemID', '')
+change_note = os.getenv('changeNote', 'Test')
 steam_username = os.getenv('steam_username', '')
-steam_password = os.getenv('steam_password', '')
 config_vdf = os.getenv('configVdf', '')
 
 # make manifest file with metadata
@@ -26,6 +25,20 @@ manifest_content = f'''"workshopitem"
     "contentfolder" "{content_root}"
     "changenote" "{change_note}"
 }}
+'''
+
+# reference file
+'''
+"workshopitem"
+{
+    "appid"        "281990"
+    "contentfolder"        "C:\\Users\\...\\my_awesome_mod"
+    "previewfile"        "C:\\Users\\...\\my_awesome_mod\\thumbnail.png"
+    "visibility"        "2"
+    "title"        "My awesome mod uploaded using SteamCmd"
+    "description"        "New description."
+    "changenote"        "Initial Release."
+}
 '''
 
 with open(manifest_path, 'w') as manifest_file:
@@ -45,32 +58,22 @@ def run_command(command):
 
 # handling SteamGuard authentication
 if not config_vdf:
-    command = f'steamcmd +set_steam_guard_code "{steam_totp}" +login "{steam_username}" "{steam_password}" +quit'
-    if run_command(command):
-        print("Successful login")
-    else:
-        print("FAILED login")
-        sys.exit(1)
+    print("Config VDF input is missing or incomplete! Cannot proceed.")
+    sys.exit(1)
+
+os.makedirs(os.path.join(steam_home, 'config'), exist_ok=True)
+
+config_path = os.path.join(steam_home, 'config', 'config.vdf')
+with open(config_path, 'wb') as config_file:
+    config_file.write(base64.b64decode(config_vdf))
+os.chmod(config_path, 0o777)
+
+command = f'steamcmd +login "{steam_username}" +quit'
+if run_command(command):
+    print("Successful login")
 else:
-    if not config_vdf:
-        print("Config VDF input is missing or incomplete! Cannot proceed.")
-        sys.exit(1)
-
-    steam_totp = "INVALID"
-
-    os.makedirs(os.path.join(steam_home, 'config'), exist_ok=True)
-
-    config_path = os.path.join(steam_home, 'config', 'config.vdf')
-    with open(config_path, 'wb') as config_file:
-        config_file.write(base64.b64decode(config_vdf))
-    os.chmod(config_path, 0o777)
-
-    command = f'steamcmd +login "{steam_username}" +quit'
-    if run_command(command):
-        print("Successful login")
-    else:
-        print("FAILED login")
-        sys.exit(1)
+    print("FAILED login")
+    sys.exit(1)
 
 # upload the item
 upload_command = f'steamcmd +login "{steam_username}" +workshop_build_item "{manifest_path}" +quit'
